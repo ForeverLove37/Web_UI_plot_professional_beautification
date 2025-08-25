0135161ff4370a1da6df3d42c1c2d2e57058cde3
ForeverLove37 <agnesgenesis37@gmail.com>
2025-08-26 00:04:39 +0800

    Stable Version 0.91

--- academicplot.py
+++ academicplot.py
@@ -45,7 +45,7 @@ def main():
     os.makedirs(app.config['OUTPUT_FOLDER'], exist_ok=True)
 
     # use waitress.serve to run application
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='127.0.0.1', port=5000)
 
 if __name__ == "__main__":
     main()
